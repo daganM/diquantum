0b1c1476d0a391360170546c9f429cbf6f1e2c35
Ambre Palanque <ambre.palanque@hotmail.com>
2017-01-25 15:24:21 +0100

    accueil done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,28 +16,15 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
-// store filter for each group
-var filters = {};
-var $grid = $('.grid').isotope({
-  itemSelector: 'filter-element'
+// On attend que la page soit chargée
+jQuery(document).ready(function()
+{
+   // On cache la zone de texte
+   jQuery('#toggle').hide();
+   // toggle() lorsque le lien avec l'ID #toggler est cliqué
+   jQuery('a#toggler').click(function()
+  {
+      jQuery('#toggle').toggle(400);
+      return false;
+   });
 });
-$('.filters').on( 'click', '.button', function() {
-  var $this = $(this);
-  // get group key
-  var $buttonGroup = $this.parents('.button-group');
-  var filterGroup = $buttonGroup.attr('data-filter-group');
-  // set filter for group
-  filters[ filterGroup ] = $this.attr('data-filter');
-  // combine filters
-  var filterValue = concatValues( filters );
-  $grid.isotope({ filter: filterValue });
-});
-
-// flatten object by concatting values
-function concatValues( obj ) {
-  var value = '';
-  for ( var prop in obj ) {
-    value += obj[ prop ];
-  }
-  return value;
-}
