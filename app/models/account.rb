58323a1d8a9c9f14dde09fed9e2796b60579336f
theopontoizeau <pontoizeau.theo@gmail.com>
2017-05-18 11:10:09 +0200

    forum

--- app/models/account.rb
+++ app/models/account.rb
@@ -23,5 +23,5 @@ class Account < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_and_belongs_to_many :articles
+        has_and_belongs_to_many :articles
 end
